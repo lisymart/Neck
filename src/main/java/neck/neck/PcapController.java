92966188a2bf678a1baf8c7f748a0df3eafd47a4
lisymart <lisymart@gmail.com>
2016-02-22 14:13:13 -0800

    update

--- src/main/java/neck/neck/PcapController.java
+++ src/main/java/neck/neck/PcapController.java
@@ -3,9 +3,12 @@ package neck.neck;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.TreeSet;
 import java.util.concurrent.Future;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -16,6 +19,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
@@ -24,30 +28,26 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class PcapController {    
 	@Autowired
 	private LogstashProcessService lps;
-   
-    @RequestMapping(value = "/pcap", method = RequestMethod.POST)
-    	public String process(HttpServletRequest request,@RequestParam final String addition) 
+	
+	private Map<String, String[]> requestMap;
+	private TreeSet<String> params = new TreeSet<>();;
+    private TreeSet<String> rename = new TreeSet<>();;
+    private TreeSet<String> fileNames = new TreeSet<>();;
+    private List<Future<String>> results ;
+	
+    @RequestMapping(value = "/pcap", method = RequestMethod.POST, params="uploadToES")
+    	public String uploadToES(HttpServletRequest request,@RequestParam final String addition) 
     			throws ServletException, IOException, InterruptedException {
-            Map<String, String[]> params = request.getParameterMap();
-            final TreeMap<String, String> change = new TreeMap<String, String>();
-            ArrayList<String> fileNames = new ArrayList<>();
-            
-            
-            for (String attName : params.keySet()){
-            	if (attName.endsWith(".pcap") || attName.endsWith(".csv") || attName.endsWith(".log")){
-            		fileNames.add(attName);
-            	} else {
-            		change.put(attName, params.get(attName)[0]);
-            	}
-            }
-            change.remove("addition");
-        
-        List<Future<String>> results = new ArrayList<>();
+    	TreeMap<String, String> renaming = new TreeMap<String, String>();
+        for (String s : rename){
+        	renaming.put(s, request.getParameter(s));
+        }
+        results = new ArrayList<>();
         
         for (String name : fileNames){
         	File folder = new File("data/pendings/" + name);
         	for (File fileName : folder.listFiles()){        
-        		results.add(lps.process(fileName, change, addition));
+        		results.add(lps.process(fileName, renaming, addition));
         	}
         }
         
@@ -71,5 +71,38 @@ public class PcapController {
         }
         
         return "success";
+    }
+    
+    @RequestMapping(value = "/pcap", method = RequestMethod.POST, params="rename")
+	public ModelAndView rename(HttpServletRequest request){
+    	if (params.isEmpty()) {
+    		requestMap = request.getParameterMap();
+    		init();
+    	}
+        String[] checked = request.getParameterValues("checked");
+        for (String att : checked){
+        	rename.add(att);
+        }
+        params.removeAll(Arrays.asList(checked));
+        Map<String, Object> model = new HashMap<>();
+        model.put("fileNames", fileNames);
+        model.put("renameList", rename);
+        model.put("attributesList", params);
+        return new ModelAndView("pcap", model);
+    }
+     
+    public void init(){
+    	for (String s: requestMap.get("attributes")){
+    		params.add(s);
+    	}
+    	fileNames();
+    }
+    
+    public void fileNames(){
+    	for (String attName : requestMap.keySet()){
+        	if (attName.endsWith(".pcap") || attName.endsWith(".csv") || attName.endsWith(".log")){
+        		fileNames.add(attName);
+        	}
+        }
     }
 }
--- src/main/webapp/WEB-INF/jsp/pcap.jsp
+++ src/main/webapp/WEB-INF/jsp/pcap.jsp
@@ -3,45 +3,48 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
-<link href="${pageContext.request.contextPath}/resources/styles.css" rel="stylesheet" type="text/css"/>
-<title> Neck </title>
+	<meta charset="UTF-8">
+	<link href="${pageContext.request.contextPath}/resources/styles.css" rel="stylesheet" type="text/css"/>
+	<title> Neck </title>
 </head>
 <body>
-<div id="center">
-<h1> Neck </h1>
-<h5> Here are selected attributes from the first 1000 lines of each log. You can change the name of attributes here: </h5>
-<br>
-<form action="/Neck/pcap" method="POST"> 
-<h5> Selected file(s): </h5>
-<c:forEach var="name" items="${fileNames}">
-<h5> ${name} </h5>
-<input type="hidden" name="${name}">
-</c:forEach>
-<br>
-<table class="table1">
-<tr>
-<td><b> Attribute orig name </b></td>
-<td><b> Your change </b></td>
-</tr>
-<tr></tr>
-<c:forEach var="item" items="${attributesList}">
-<tr>
-<td> ${item} </td>
-<td> <input type="text" name="${item}"><br> </td>
-</tr> 
-</c:forEach>
-</table>
-<h5> And here you can add any other logstash configuration. Please use the whole filter configuration e.g.:  </h5>
-<i> mutate{rename => {"fieldName" => "newName"}} </i>
-<br><br>
-<textarea class="addition" name="addition" rows=10>
-</textarea>
-<br>
-<br>
-<input class="btn" type="submit" value="Upload to ES">
-</form>
-</div>
+	<div id="center">
+		<h1> Neck </h1>
+		<h4> Here are selected attributes from the first 1000 lines of each log. You can modify them. </h4>
+		<c:forEach var="name" items="${fileNames}">
+			<a>- ${name} -</a>
+			<input type="hidden" name="${name}">
+		</c:forEach>
+		<br><br>
+		<form action="/Neck/pcap" method="POST"> 
+			<input class="btn" name="rename" type="submit" value="Rename">
+			<br><br>
+			<div> Choose attributes to process: </div>
+			<ul>	
+				<c:forEach var="item" items="${attributesList}">
+					<li> <input type="checkbox" name="checked" value="${item}"> ${item} </li>
+					<input type="hidden" name="attributes" value="${item}">
+				</c:forEach>
+			</ul>
+			<br>
+			<h4> Renaming: </h4>
+			<table class="table1">
+			<tr><td> Original name </td><td> Your name </td></tr>	
+				<c:forEach var="item" items="${renameList}">
+					<tr> <td>${item}</td> <td> <input type="text" name="${item}"></td> </tr>
+				</c:forEach>
+			</table>
+			<h5> And here you can add any other logstash configuration. Please use the whole filter configuration e.g.:  </h5>
+			<i> mutate{rename => {"fieldName" => "newName"}} </i>
+			<br>
+			<br>
+			<textarea class="addition" name="addition" rows=10>
+			</textarea>
+			<br>
+			<br>
+			<input class="btn" name="uploadToES" type="submit" value="Upload to ES">
+		</form>
+	</div>
 </body>
 </html>
 
