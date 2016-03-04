d9f762e7c97174643b5fbe9a48cac8f17237136e
lisymart <lisymart@gmail.com>
2016-03-04 05:50:26 -0800

    upload

---
+++ src/main/webapp/resources/scripts.js
@@ -0,0 +1,75 @@
+var fieldName='checked';
+
+function selectall(){
+  var i=document.frm.elements.length;
+  var e=document.frm.elements;
+  var name=new Array();
+  var value=new Array();
+  var j=0;
+  for(var k=0;k<i;k++)
+  {
+    if(document.frm.elements[k].name==fieldName)
+    {
+      if(document.frm.elements[k].checked==true){
+        value[j]=document.frm.elements[k].value;
+        j++;
+      }
+    }
+  }
+  checkSelect();
+}
+function selectCheck(obj)
+{
+ var i=document.frm.elements.length;
+  for(var k=0;k<i;k++)
+  {
+    if(document.frm.elements[k].name==fieldName)
+    {
+      document.frm.elements[k].checked=obj;
+    }
+  }
+  selectall();
+}
+
+function selectallMe()
+{
+  if(document.frm.allCheck.checked==true)
+  {
+   selectCheck(true);
+  }
+  else
+  {
+    selectCheck(false);
+  }
+}
+function checkSelect()
+{
+ var i=document.frm.elements.length;
+ var berror=true;
+  for(var k=0;k<i;k++)
+  {
+    if(document.frm.elements[k].name==fieldName)
+    {
+      if(document.frm.elements[k].checked==false)
+      {
+        berror=false;
+        break;
+      }
+    }
+  }
+  if(berror==false)
+  {
+    document.frm.allCheck.checked=false;
+  }
+  else
+  {
+    document.frm.allCheck.checked=true;
+  }
+}
+
+function ShowLoading(e) {
+    	document.getElementById('spinner').style.display = '';
+    	document.getElementById('overlay').style.display = '';
+    	e.stopPropagation();
+}
+
